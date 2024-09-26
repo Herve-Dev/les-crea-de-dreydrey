a4f91adf1beb22f270a02a8277ad311d469f2ec3
FullMetal-Dev <herve.developper@gmail.com>
2024-09-26 06:13:54 +0200

    add banner with image

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,27 @@
 <template>
     <nav class="navbar"> 
-        <ul class="navbar__list">  
-            <li class="navbar__item">  
+        <ul class="navbar__list"> 
+            <div class="navbar__center">
+                <li class="navbar__item">  
                 <NuxtLink to="/" class="navbar__link">Accueil</NuxtLink>  
-            </li>
-            <li class="navbar__item">
-                <NuxtLink to="/" class="navbar__link">Catégorie</NuxtLink>
-            </li>
-            <li class="navbar__item">
-                <NuxtLink to="/contact" class="navbar__link">Contact</NuxtLink>
-            </li>
+                </li>
+                <li class="navbar__item">
+                    <NuxtLink to="/" class="navbar__link">Catégorie</NuxtLink>
+                </li>
+                <li class="navbar__item">
+                    <NuxtLink to="/contact" class="navbar__link">Contact</NuxtLink>
+                </li>
+            </div> 
+            
+            <!-- Ajouter quand backend fait -->
+            <!-- <div class="navbar__right">
+                <li class="navbar__item">
+                    <NuxtLink to="/contact" class="navbar__link">Connexion</NuxtLink>
+                </li>
+                <li class="navbar__item">
+                    <NuxtLink to="/contact" class="navbar__link">S'enregister</NuxtLink>
+                </li>
+            </div> -->
         </ul>
     </nav>
 </template>
