da681b6ca2c8ea193b7989f89af3d9be7afbcb87
FullMetal-Dev <herve.developper@gmail.com>
2024-10-01 21:41:30 +0200

    work on footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,22 +1,27 @@
 <template>
     <footer>
-        <section>
+        <section class="section-logo">
             <ul>
                 <li>
-                    <h4>
-                       <u>Suivez-moi:</u> 
-                    </h4>
+                    <a href="https://www.instagram.com/les_crea_de_dreydrey_?igsh=MW9rYWd3cjZwdm4waQ==">
+                       <img src="/assets/images/icons8-instagram-100.png" alt="logo d'instagram"> 
+                    </a>
                 </li>
                 <li>
-                    <img src="/assets/images/icons8-instagram-100.png" alt="logo d'instagram">
-                </li>
-                <li>
-                    <img src="/assets//images/icons8-facebook-100.png" alt="logo de facebook">
+                    <a href="https://www.facebook.com/les.crea.de.dreydrey">
+                       <img src="/assets//images/icons8-facebook-100.png" alt="logo de facebook"> 
+                    </a>
                 </li>
             </ul>
         </section>
-        <section>
-
+        <section class="section-copyright">
+            <p>
+                © 2024 Les Créa De DreyDrey 
+                Tous droits réservés
+            </p>
+            <p>
+                Site développé par | FullMetalDev 
+            </p>
         </section>
     </footer>
 </template>
