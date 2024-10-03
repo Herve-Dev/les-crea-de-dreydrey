97c3355442296e72f4ac62219a5b901ed4248994
FullMetal-Dev <herve.developper@gmail.com>
2024-10-03 20:20:05 +0200

    add carousel nuxt

--- components/Modal.vue
+++ components/Modal.vue
@@ -9,7 +9,9 @@
         <img class="modal-img" src="/assets/images/pot_graphique.webp" alt="Photo d'un pot graphique en blanc et or">
         <h2 id="modal-title">{{ title }}</h2>
         <p> <u>Description:</u> </p>
-        <p id="modal-body">{{ body }} Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus eaque delectus quibusdam praesentium necessitatibus laboriosam consectetur ipsam vel porro quo consequatur fuga doloremque eius officiis, molestias deserunt? Debitis soluta minus illo sed, in ratione impedit nulla dolor, iste aut repellat? Aperiam odio repudiandae, modi vitae rerum deleniti veritatis, amet reiciendis voluptatem nam dignissimos velit? Exercitationem aliquid esse aliquam facilis ipsa reiciendis at, inventore libero cupiditate quidem. Consequatur reprehenderit atque voluptatibus est quae sint debitis voluptate omnis fugit numquam illum ex nostrum amet, sit blanditiis tempora cumque sequi! Tempore in laboriosam mollitia, ea id, deleniti fugiat aperiam quae eum, eaque placeat?</p>
+        <p id="modal-body">
+          {{ body }} Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus eaque delectus quibusdam praesentium necessitatibus laboriosam consectetur ipsam vel porro quo consequatur fuga doloremque eius officiis, molestias deserunt? Debitis soluta minus illo sed, in ratione impedit nulla dolor, iste aut repellat? Aperiam odio repudiandae, modi vitae rerum deleniti veritatis, amet reiciendis voluptatem nam dignissimos velit? Exercitationem aliquid esse aliquam facilis ipsa reiciendis at, inventore libero cupiditate quidem. Consequatur reprehenderit atque voluptatibus est quae sint debitis voluptate omnis fugit numquam illum ex nostrum amet, sit blanditiis tempora cumque sequi! Tempore in laboriosam mollitia, ea id, deleniti fugiat aperiam quae eum, eaque placeat?
+        </p>
         <p> 
           <u>Prix:</u> 
           49euro 
